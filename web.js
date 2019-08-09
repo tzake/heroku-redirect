75f9cca17b3b29900ca5f114b1fcc0deec2194ea
tzake <mtatarca@gmail.com>
2019-08-09 19:05:43 +0300

    Update web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var newBaseURL = process.env.NEW_BASE_URL || 'https://splitboardromania.ro';
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
